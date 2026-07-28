d724029510251fa341997a37bf5b50bfc1fcd4ec
wangbaoqing <wbq6587@163.com>
2026-07-28 09:21:22 +0800

    AI生成应用模块

---
+++ src/test/java/com/wbq/aicodemother/ai/AiCodeGeneratorServiceTest.java
@@ -0,0 +1,29 @@
+package com.wbq.aicodemother.ai;
+
+import com.wbq.aicodemother.ai.model.HtmlCodeResult;
+import com.wbq.aicodemother.ai.model.MultiFileCodeResult;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class AiCodeGeneratorServiceTest {
+
+    @Resource
+    private AiCodeGeneratorService aiCodeGeneratorService;
+
+    @Test
+    void generateHtmlCode() {
+        HtmlCodeResult result = aiCodeGeneratorService.generateHtmlCode("做个简易的博客网站，不超过20行");
+        Assertions.assertNotNull(result);
+    }
+
+    @Test
+    void generateMultiFileCode() {
+        MultiFileCodeResult result = aiCodeGeneratorService.generateMultiFileCode("做个简易的留言板网站，不超过50行");
+        Assertions.assertNotNull(result);
+    }
+}
